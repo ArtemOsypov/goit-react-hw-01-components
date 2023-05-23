b8e4c685a8baef802271112e539f2313fcc0bcba
ArtemOsypov <artem.osypov@gmail.com>
2023-05-23 23:29:14 +0300

    DZ-1.1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,56 +1,50 @@
 // { username, tag, location, avatar, stats: { followers, views, likes } }
 
 import PropTypes from 'prop-types';
-// import user from 'user.json';
-// const user = { username, tag, location, avatar, stats: { followers, views, likes } }
+import { ProfileCard, Description, Avatar, Name, Tag, Location, Stats, StatsItem, Label, Quantity   } from './Profile.styled';
+
 export const Profile = ({ username, tag, location, avatar, stats }) => {
 
   // console.log(username);
   // console.log(stats.followers);
   // console.log(card.stats.followers);
   return (
- <div class="profile">
-      <div class="description">
-        <img
+    <ProfileCard>
+      <Description>
+        <Avatar 
           src={avatar}
           alt={username}
-          class="avatar"
         />
-        <p class="name">{username}</p>
-        <p class="tag">{tag}</p>
-       <p class="location">S{location}</p>
+        <Name>{username}</Name>
+        <Tag>{tag}</Tag>
+        <Location>S{location}</Location>
        
-      </div> 
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-         <span class="quantity">{stats.followers}</span>
-        </li>
-        <li>
-          <span class="label">Views</span>
-         <span class="quantity">{stats.views}</span>
-        </li>
-        <li>
-          <span class="label">Likes</span>
-         <span class="quantity">{stats.likes}</span>
-        </li>
-      </ul>
-    </div>,
-
+      </Description>
+      <Stats>
+        <StatsItem >
+          <Label>Followers</Label>
+          <Quantity>{stats.followers}</Quantity>
+        </StatsItem >
+        <StatsItem >
+          <Label>Views</Label>
+          <Quantity>{stats.views}</Quantity>
+        </StatsItem >
+        <StatsItem>
+          <Label>Likes</Label>
+          <Quantity>{stats.likes}</Quantity>
+        </StatsItem>
+      </Stats>
+    </ProfileCard>
+    );
+};
 Profile.propTypes = {
-    username: PropTypes.string,
-    tag: PropTypes.string,
-    location: PropTypes.string,
-    avatar: PropTypes.string,
-    stats: PropTypes.exact({
-      followers: PropTypes.number.isRequired,
-      views: PropTypes.number.isRequired,
-      likes: PropTypes.number.isRequired,
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
   }),
-}
-
-    
-  )
-  
-
-}
+};
